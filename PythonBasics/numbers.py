1cca4bd037c847ec12c9dfb10bdc1c00422fb9ff
Sreehari B. Hegden <sreeharihegden@users.noreply.github.com>
2021-01-10 20:42:14 -0500

    Variables in Python.

--- PythonBasics/numbers.py
+++ PythonBasics/numbers.py
@@ -1,11 +1,22 @@
 
+# Addition
+print(2 + 2)
+
+print(3 - 2)
+
+print(3 * 2)
+
+print(100 / 3) # Floating point number
 
 print(29/3)
 
-print(29%3)
+# Remainder
+print(29 % 3)
 
 print(2+5 * 5+2)
 
 print((2+5) * (5+2))
+
+print(3 * 10 + 8 -5)
 
 print(10 ** 3) # Power of
---
+++ PythonBasics/variables.py
@@ -0,0 +1,33 @@
+"""
+Variables!
+"""
+
+exampleVar = 5
+print(exampleVar)
+
+# Calculate Area
+print(10 * 5)
+width = 10
+length = 5
+print(width * length)
+
+area = width * length
+print(area)
+
+# Legal Variable Names
+exampleVar = 'Python'
+example_var = 'Python'
+_example_var = 'Python' # Must start with a letter or _.
+EXAMPLEVAR = 'Python'
+exampleVar2 = 'Python'
+
+# Illegal Variable Names
+"""
+2exampleVar = 'Python' # Cannot start with number
+example-var = 'Python' # Cannot have Minus symbol
+example var = 'Python' # Cannot have Space
+"""
+
+# Python variables should follow snake_case
+first_name = 'Erix' # Words separated by underscore.
+
