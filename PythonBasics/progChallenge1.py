0a8dadb7fb1432716f2960900906b3150d87c852
Sreehari B. Hegden <sreeharihegden@users.noreply.github.com>
2021-01-10 20:57:04 -0500

    Programming Challenge 1.

---
+++ PythonBasics/progChallenge1.py
@@ -0,0 +1,32 @@
+
+# Programming Challenge 1
+
+# Subtracts each item from bank_amount
+# and prints the remaining bank_amount.
+
+'''
+Subtracts each item from bank_amount
+and prints the remaining bank_amount.
+'''
+
+"""
+Subtracts each item from bank_amount 
+and prints the remaining bank_amount.
+"""
+
+bank_amount = 100
+
+item_one = 25
+item_two = 30
+item_three = 15
+
+# print(bank_amount - item_one)
+bank_amount -= item_one
+
+# print(bank_amount - item_two)
+bank_amount -= item_two
+
+# print(bank_amount - item_three)
+bank_amount -= item_three
+
+print(bank_amount)
