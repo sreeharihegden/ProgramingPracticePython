76d13b8af3ad9ff5c8ca0b10501e8b0d920da969
Sreehari B. Hegden <sreeharihegden@users.noreply.github.com>
2021-01-10 20:45:37 -0500

    Comments in Python.

---
+++ PythonBasics/comments.py
@@ -0,0 +1,21 @@
+"""
+Comments!
+"""
+
+# A comment
+print("Hello Python!")
+
+'''
+Multi-
+Line
+Comments
+'''
+
+"""
+Multi-
+Line
+Comments also with
+Double Quotes.
+"""
+
+# print("Hello World!") Comments also prevent from execution
