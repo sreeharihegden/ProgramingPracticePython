1c1bfbb969066629092c2dbab81204f44459239f
Sreehari B. Hegden <sreeharihegden@users.noreply.github.com>
2021-01-10 21:07:08 -0500

    Python Strings Part 1.

---
+++ PythonBasics/strings.py
@@ -0,0 +1,24 @@
+"""
+Strings!
+"""
+
+print('Hello World!')
+print("Hello World!")
+
+# Escape Character
+print('You\'re Welocme')
+print("You\'re\nWelcome")
+
+statement = 'Hello Python!'
+print(statement)
+
+# Multi line statements without \n
+statement = '''
+Attention
+Everyone
+This
+Is
+Very
+Important
+'''
+print(statement)
